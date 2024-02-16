52003819255ccb723f2b27e77a24ed863c9b1351
Kaesyo Félix <129511975+KARAUJO1003@users.noreply.github.com>
2024-02-16 14:58:39 -0300

    FEAT: Formato currency e number.

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,32 @@
 import { useState } from "react";
-import { LuCaseLower, LuCaseUpper, LuCopy, LuCheck  } from "react-icons/lu";
+import { LuCaseLower, LuCaseUpper, LuCopy, LuCheck } from "react-icons/lu";
 import { Button } from "../@/components/ui/button";
 import { toast } from "sonner";
 import copy from "copy-to-clipboard";
 import extenso from "extenso";
 import { dynamicMask } from "simple-currency-mask";
-import { mask, unmask } from 'simple-currency-mask';
+import { Switch } from "../@/components/ui/switch";
+import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "../@/components/ui/select";
 
 function App() {
-  const [valueInput, setValueInput] = useState("R$ 0.00");
+  const [valueInput, setValueInput] = useState();
   const [result, setResult] = useState();
-  const [ currencyValue, setCurrencyValue] = useState()
   const [textChange, setTextChange] = useState(true);
   const [toggleCopy, setToggleCopy] = useState(false)
+  const [mode, setMode] = useState(true)
 
   function AddNewValue() {
     if (valueInput !== "") {
       // Remover caracteres não numéricos, exceto a vírgula
       const cleanValue = valueInput.replace(/[^\d,]/g, '');
-      
       // Chamar a função extenso com o valor como uma string
-      const newValue = extenso(cleanValue, { mode: "currency" });
+      const newValue = extenso(cleanValue, { mode: mode ? "currency" : "number" });
       setResult(newValue);
-      setCurrencyValue(newValue)
     } else {
       alert("Digite algo");
     }
   }
-  
-  
-  
-  
-  
+
   function handleEnterKey(e) {
     if (e.key === "Enter") {
       e.preventDefault();
@@ -41,18 +36,16 @@ function App() {
 
   function handleCopy() {
     let copiedText = result;
-
     // Verifica a formatação desejada (maiúsculas ou minúsculas)
     if (textChange) {
       copiedText = copiedText.toUpperCase();
     } else {
       copiedText = copiedText.toLowerCase();
     }
-
     copy(copiedText);
     toast("Seu texto foi copiado com sucesso!", {
       description: `${copiedText}`,
-      variant: 'outline', 
+      variant: 'outline',
       action: {
         label: "Fechar",
         onClick: () => setToggleCopy(false),
@@ -61,10 +54,17 @@ function App() {
   }
 
   function changeMonetaryValue(e) {
-    let config = { decimalSeparator: ",", currency: "R$", negative: true };
+    if (mode) {
 
-    setValueInput(dynamicMask(e.target.value, config));
+      let config = { decimalSeparator: ",", currency: 'R$',  negative: true };
+
+      setValueInput(dynamicMask(e.target.value, config));
+    } else {
+
+      setValueInput(e.target.value)
+    }
   }
+
   return (
     <div className=" bg-zinc-900 h-screen min-w-full m-0 p-0 max-sm:p-2 text-zinc-100 flex items-center justify-center flex-col">
       <div className="flex flex-col gap-3 w-full items-center justify-center">
@@ -84,44 +84,72 @@ function App() {
           >
             Converter
           </Button>
+
         </div>
         <div
-          className={`p-0  bg-zinc-800 border border-zinc-700 mt-3 max-sm:w-full w-96 rounded-md ${
-            textChange ? "uppercase" : "lowercase"
-          } text-sm `}
+          className={`p-0  bg-zinc-800 border border-zinc-700 mt-3 max-sm:w-full w-96 rounded-md ${textChange ? "uppercase" : "lowercase"
+            } text-sm `}
         >
-          <div className="flex items-center justify-between px-5 py-3">
-            <div className="gap-2 border border-zinc-700 hover:border-indigo-400 transition-all rounded-md overflow-hidden">
-              <Button
-                onClick={() => setTextChange(false)}
-                value={textChange}
-                className="bg-transparent p-2  h-auto rounded-none focus:text-indigo-400 focus:bg-zinc-600 outline-0 focus:outline-none border-none transition-all hover:bg-zinc-700"
-              >
-                <LuCaseLower size={18} />
-              </Button>
+          <div className="flex items-center justify-between px-5 h-20">
+            <div className="flex items-center justify-between w-full h-8">
 
-              <Button
-                onClick={() => setTextChange(true)}
-                className="bg-transparent p-2  h-auto rounded-none focus:text-indigo-400 hover:border-indigo-400 focus:bg-zinc-600 outline-0 focus:outline-none border-none transition-all hover:bg-zinc-700"
-              >
-                <LuCaseUpper size={18} />
-              </Button>
+              <div className=" flex items-centergap-2 border border-zinc-700 hover:border-indigo-400 transition-all rounded-md overflow-hidden">
+                <Button
+                  onClick={() => setTextChange(false)}
+                  value={textChange}
+                  className="bg-transparent p-1  h-8 w-8 rounded-none focus:text-indigo-400 focus:bg-zinc-600 outline-0 focus:outline-none border-none transition-all hover:bg-zinc-700"
+                >
+                  <LuCaseLower size={18} />
+                </Button>
+
+
+                <Button
+                  onClick={() => setTextChange(true)}
+                  className="bg-transparent p-1  h-8 w-8 rounded-none focus:text-indigo-400 hover:border-indigo-400 focus:bg-zinc-600 outline-0 focus:outline-none border-none transition-all hover:bg-zinc-700"
+                >
+                  <LuCaseUpper size={18} />
+                </Button>
+
+
+              </div>
+              <Select onValueChange={(() => setMode(!mode))} defaultValue={mode}>
+                <SelectTrigger className="w-[180px] border-zinc-700 h-8">
+                  <SelectValue />
+                </SelectTrigger>
+                <SelectContent className='mt-1 border-zinc-700' >
+                  <SelectGroup className='w-[170px] text-background bg-zinc-700 divide-y divide-zinc-600'>
+                    <SelectItem value={true} className='p-2 cursor-pointer border-zinc-800 bg-zinc-700 hover:bg-zinc-600/80 text-xs rounded-none' >Monetário</SelectItem>
+                    <SelectItem value={false} className='p-2 cursor-pointer border-zinc-800 bg-zinc-700 hover:bg-zinc-600/80 text-xs border-b  rounded-none' >Numero</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+
+              <div>
+                <Button
+                  onClick={(e) => handleCopy(e) + setToggleCopy(true)}
+                  className="w-8 h-8 flex items-center justify-center bg-zinc-700 focus:outline-none hover:bg-zinc-600 focus:text-indigo-400"
+                >
+                  {toggleCopy ? <LuCheck /> : <LuCopy size={14} />}
+
+                </Button>
+              </div>
             </div>
 
-            <div>
-              <Button
-                onClick={(e) => handleCopy(e)  + setToggleCopy(true)}
-                className="p-2 h-auto bg-zinc-700 focus:outline-none hover:bg-zinc-600 focus:text-indigo-400"
-              >
-                {toggleCopy ? <LuCheck/> : <LuCopy className="w-3 h-3" />}
-                
-              </Button>
-            </div>
+
           </div>
-          <p className="p-5 text-start font-family:ebrima min-h-20 border-t border-zinc-700 text-zinc-300">
-             {  valueInput + ` (${result})`}
-            
-          </p>
+          <div className="min-h-28 border-t border-zinc-700">
+            {result && (
+              <p className="p-5 text-start font-family:ebrima min-h-20  text-zinc-300">
+                {mode ? <><span className="uppercase">{valueInput} </span>
+                  <span>({result}).</span>
+                </> :
+                  <span>{result}</span>
+                }
+
+
+              </p>)}
+          </div>
+
           <Button
             className="hidden max-sm:block h-10 px-5 py-0 font-bold text-sm rounded-none rounded-r-md bg-indigo-600 hover:bg-indigo-500 transition-all max-sm:rounded max-sm:py-3 max-sm:w-full"
             onClick={AddNewValue}
